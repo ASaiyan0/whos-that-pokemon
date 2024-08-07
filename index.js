6e062ed0aa0fe4fd5aabc44ee6dbe9fda7e8d172
unknown <asaya30@gmail.com>
2024-08-06 21:59:14 -0400

    All functionality should be working

--- index.js
+++ index.js
@@ -4,49 +4,141 @@ window.addEventListener("load", () => {
   const eqButton = document.querySelector(".eButton");
   const numDisplay = document.querySelector(".numDisplay");
   const opArray = ["add", "subtract", "multiply", "divide"];
-  let currentString = numDisplay.textContent;
+  let currentString = "";
+  //This should always have STRING data. numDisplay, despite the name, should also always have STRING data.
   let firstNum = "";
+  //This should always have NUMBER data, OR a BLANK STRING (bad practice - figure out a better solution later).
   let currentOp = "";
+  //This is STRING.
+  let clearState = "ac";
+  //This is STRING.
   let errLock = false;
+  //This is BOOLEAN.
+
+  function clearCheck() {
+    //There's gotta be a more elegant way of doing this, right?
+    if (currentString != "") {
+      clearState = "c";
+      opButtons[5].textContent = clearState;
+    } else {
+      clearState = "ac";
+      opButtons[5].textContent = clearState;
+    }
+  }
+
+  function rounder(num) {
+    //This function accepts NUMBER data and returns STRING data.
+    if (Math.abs(num) > 999999999 || Math.abs(num) < 0.00000001) {
+      return num.toExponential(5);
+    } else {
+      let n = 10 - num.toString().split(".")[0].match(/\d/g).length;
+      //line 16 reads as follows: "Subtract the number of digits to the left of the decimal point from 10"
+      return parseFloat(num.toFixed(n)).toString();
+    }
+  }
+
+  function equation() {
+    if (currentOp == "add") {
+      currentString = firstNum + Number(currentString);
+      numDisplay.textContent = rounder(Number(currentString));
+      firstNum = Number(currentString);
+      currentOp = "";
+      currentString = "";
+    } else if (currentOp == "subtract") {
+      currentString = firstNum - Number(currentString);
+      numDisplay.textContent = rounder(Number(currentString));
+      firstNum = Number(currentString);
+      currentOp = "";
+      currentString = "";
+    } else if (currentOp == "multiply") {
+      currentString = firstNum * Number(currentString);
+      numDisplay.textContent = rounder(Number(currentString));
+      firstNum = Number(currentString);
+      currentOp = "";
+      currentString = "";
+    } else if (currentOp == "divide") {
+      if (Number(currentString) == 0) {
+        numDisplay.textContent = "Error";
+        firstNum = "";
+        currentOp = "";
+        currentString = "";
+        errLock = true;
+      } else {
+        currentString = firstNum / Number(currentString);
+        numDisplay.textContent = rounder(Number(currentString));
+        firstNum = Number(currentString);
+        currentOp = "";
+        currentString = "";
+      }
+    } else {
+      return;
+    }
+  }
 
   for (let n = 0; n <= 9; n++) {
     numButtons[n].addEventListener("click", () => {
-      if (currentString.length <= 10) {
-        if (errLock == true) {
-          currentString = "";
-          errLock = false;
-          currentString = currentString + numButtons[n].textContent;
-          numDisplay.textContent = currentString;
-        } else {
-          currentString = currentString + numButtons[n].textContent;
-          numDisplay.textContent = currentString;
-        }
+      if (errLock == true) {
+        errLock = false;
+        currentString = numButtons[n].textContent;
+        numDisplay.textContent = currentString;
+        clearCheck();
+      } else if (currentString == "") {
+        currentString = numButtons[n].textContent;
+        numDisplay.textContent = currentString;
+        clearCheck();
+      } else if (currentString == "" || currentString == "0") {
+        currentString = numButtons[n].textContent;
+        numDisplay.textContent = currentString;
+        clearCheck();
+      } else if (currentString.match(/\d/g).length < 10) {
+        //If the currentString contains less than 10 digits (excluding decimal point and negative sign)
+        currentString = currentString + numButtons[n].textContent;
+        numDisplay.textContent = currentString;
+        clearCheck();
       } else {
-        return;
+        currentString = currentString + numButtons[n].textContent;
+        clearCheck();
+        //Else, we will continue to add digits to the currentString, but NOT to the actual display (out of room)
       }
     });
   }
 
   numButtons[10].addEventListener("click", () => {
     if (errLock == true) {
-      return;
+      errLock = false;
+      currentString = "0" + numButtons[10].textContent;
+      numDisplay.textContent = currentString;
+      clearCheck();
     } else if (currentString.includes(".")) {
       return;
+    } else if (currentString == "") {
+      currentString = "0" + numButtons[10].textContent;
+      numDisplay.textContent = currentString;
+      clearCheck();
     } else {
       currentString = currentString + numButtons[10].textContent;
       numDisplay.textContent = currentString;
+      clearCheck();
     }
   });
 
   numButtons[11].addEventListener("click", () => {
-    if (errLock == true || currentString == "" || currentString == ".") {
+    if (errLock == true) {
       return;
-    } else if (currentString.length > 10) {
-      currentString = (Number(currentString) * -1).toExponential(5);
-      numDisplay.textContent = currentString;
+    } else if (currentString == "" && firstNum != "") {
+      currentString = (firstNum * -1).toString();
+      numDisplay.textContent = rounder(Number(currentString));
+      firstNum = Number(currentString);
+      currentOp = "";
+      currentString = "";
+      clearCheck();
     } else {
       currentString = (Number(currentString) * -1).toString();
-      numDisplay.textContent = currentString;
+      numDisplay.textContent = rounder(Number(currentString));
+      firstNum = Number(currentString);
+      currentOp = "";
+      currentString = "";
+      clearCheck();
     }
   });
 
@@ -54,37 +146,66 @@ window.addEventListener("load", () => {
     opButtons[n].addEventListener("click", () => {
       if (errLock == true) {
         return;
+      } else if (firstNum != "" && currentOp != "" && currentString != "") {
+        equation();
+        currentOp = opArray[n];
+        clearCheck();
+      } else if (firstNum != "" && currentString == "") {
+        currentOp = opArray[n];
+        clearCheck();
       } else {
         firstNum = Number(currentString);
         currentOp = opArray[n];
         currentString = "";
-        numDisplay.textContent = currentString;
+        clearCheck();
       }
     });
   }
+
   opButtons[4].addEventListener("click", () => {
     if (errLock == true) {
       return;
-    } else if (currentString.includes("-")) {
-      currentString = "Error";
-      numDisplay.textContent = currentString;
+    } else if (
+      Number(currentString) < 0 ||
+      (currentString == "" && firstNum < 0)
+    ) {
+      numDisplay.textContent = "Error";
       firstNum = "";
       currentOp = "";
+      currentString = "";
       errLock = true;
+      clearCheck();
+    } else if (currentString == "" && firstNum != "") {
+      currentString = Math.sqrt(firstNum).toString();
+      numDisplay.textContent = rounder(Number(currentString));
+      firstNum = Number(currentString);
+      currentOp = "";
+      currentString = "";
+      clearCheck();
     } else {
-      currentString = Math.sqrt(Number(currentString))
-        .toString()
-        .substring(0, 10);
-      numDisplay.textContent = currentString;
+      currentString = Math.sqrt(Number(currentString)).toString();
+      numDisplay.textContent = rounder(Number(currentString));
+      firstNum = Number(currentString);
+      currentOp = "";
+      currentString = "";
+      clearCheck();
     }
   });
 
   opButtons[5].addEventListener("click", () => {
-    currentString = "";
-    numDisplay.textContent = currentString;
-    firstNum = "";
-    currentOp = "";
-    errLock = false;
+    if (clearState == "ac") {
+      numDisplay.textContent = "0";
+      firstNum = "";
+      currentOp = "";
+      currentString = "";
+      errLock = false;
+      clearCheck();
+    } else if (clearState == "c") {
+      numDisplay.textContent = "0";
+      currentString = "";
+      errLock = false;
+      clearCheck();
+    }
   });
 
   eqButton.addEventListener("click", () => {
@@ -92,52 +213,10 @@ window.addEventListener("load", () => {
       return;
     } else if (firstNum == "" && currentString != "") {
       numDisplay.textContent = currentString;
+      clearCheck();
     } else if (firstNum != "" && currentString != "") {
-      if (currentOp == "add") {
-        currentString = (firstNum + Number(currentString))
-          .toString()
-          .substring(0, 10);
-        numDisplay.textContent = currentString;
-        firstNum = "";
-        currentOp = "";
-      } else if (currentOp == "subtract") {
-        currentString = (firstNum - Number(currentString))
-          .toString()
-          .substring(0, 10);
-        numDisplay.textContent = currentString;
-        firstNum = "";
-        currentOp = "";
-      } else if (currentOp == "multiply") {
-        if ((firstNum * Number(currentString)).toString().length > 10) {
-          currentString = (firstNum * Number(currentString)).toExponential(5);
-        } else {
-          currentString = (firstNum * Number(currentString)).toString();
-        }
-        numDisplay.textContent = currentString;
-        firstNum = "";
-        currentOp = "";
-      } else if (currentOp == "divide") {
-        if (Number(currentString) == 0) {
-          currentString = "Error";
-          numDisplay.textContent = currentString;
-          firstNum = "";
-          currentOp = "";
-          errLock = true;
-        } else {
-          currentString = (firstNum / Number(currentString))
-            .toString()
-            .substring(0, 10);
-          numDisplay.textContent = currentString;
-          firstNum = "";
-          currentOp = "";
-        }
-      } else {
-        currentString = "Error";
-        numDisplay.textContent = currentString;
-        firstNum = "";
-        currentOp = "";
-        errLock = true;
-      }
+      equation();
+      clearCheck();
     }
   });
 });
