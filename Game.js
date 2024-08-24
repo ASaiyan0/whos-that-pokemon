d540e3e04a76267a8ae35f43b350440ffdf90b6f
ASaiyan <113263644+ASaiyan0@users.noreply.github.com>
2024-08-24 09:44:20 -0400

    Update Game.js

--- Game.js
+++ Game.js
@@ -48,7 +48,7 @@ export class Game {
         await this.revealAllHints();
         this.numWrong++;
         this.wrongGuesses.textContent = "Wrong Guesses: " + this.numWrong;
-        setTimeOut(() => {this.quitNextButton.textContent = "Next Pokémon", 1500});
+        setTimeOut(() => {this.quitNextButton.textContent = "Next Pokémon"}, 1500);
       } else if (this.quitNextButton.textContent == "Next Pokémon") {
         this.newPokemon(this.randomNum());
         this.quitNextButton.textContent = "Give Up";
